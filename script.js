7d9a56831e35576e54b6c2ce7ccd9ebe1b99d95a
Jan Lochman <149424289+b0tinho@users.noreply.github.com>
2025-10-07 07:58:01 +0000

    Com1_1.2

--- script.js
+++ script.js
@@ -1,27 +1,49 @@
 (function() {
-    var ex1_button=document.getElementById('ex1_button');  
+  var ex1_button = document.getElementById('ex1_button');
 
-    var ex1_content=document.getElementById('ex1_content');  
+  var ex1_content = document.getElementById('ex1_content');
 
 
 
 
   //Ex1
 
-    ex1_button.onclick=function(){   
+  ex1_button.onclick = function() {
 
-      var tabela=[]   
+    var tabela = []
 
-      for(var i=0;i<=9;i++)   
+    for (var i = 0; i <= 9; i++) {
 
-      {     
+      tabela.push(i);
 
-        tabela.push(i);   
+    }
 
-      }     
+    ex1_content.innerHTML = tabela.toString();
 
-      ex1_content.innerHTML=tabela.toString(); 
+  }
 
-    } 
+  //Ex2
+
+  var ex2_text = document.getElementById('ex2_text');
+
+  ex2_text.oninput = function() {
+    const text = this.value;
+    const content = document.getElementById('ex2_content');
+
+    if (text.length !== 9) {
+      content.textContent = "Długość numeru musi być równa 9";
+      return;
+    }
+    if (/[a-zA-Z]/.test(text)) {
+      content.textContent = "Numer nie może zawierać liter";
+      return;
+    }
+    if (/[^0-9]/.test(text)) {
+      content.textContent = "Numer nie może zawierać znaków specjalnych";
+      return;
+    }
+    content.textContent = "Numer telefonu jest poprawny";
+  }
+
 
 })();
